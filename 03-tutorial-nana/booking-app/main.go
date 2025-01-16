d467ce65e1edc082890124fd7b37536aac6a0915
CosmoCarpincho <cosmocarpincho@gmail.com>
2025-01-16 10:38:28 -0300

    study go: nana tutorial (beginners)

--- 03-tutorial-nana/booking-app/main.go
+++ 03-tutorial-nana/booking-app/main.go
@@ -8,6 +8,8 @@ func main() {
   conferenceName := "Go Conference"
   const conferenceTickets = 50
   var remainingTickets uint = 50
+  var bookings []string
+  // bookings := []string{}
 
   fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
@@ -15,15 +17,34 @@ func main() {
   fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
   fmt.Println("Get yout tickets here to attend")
 
-  var userName string
-  var userTickets int
 
-  fmt.Scan(&userName)
+  var firstName string
+  var lastName string
+  var email string
+  var userTickets uint
 
-  userName = "Tom"
-  userTickets = 2
+  fmt.Println("Enter your first name:")
+  fmt.Scan(&firstName)
 
-  fmt.Printf("Use %v booked %v tickets.\n", userName, userTickets)
+  fmt.Println("Enter your last name:")
+  fmt.Scan(&lastName)
+
+  fmt.Println("Enter your email adrress:")
+  fmt.Scan(&email)
+
+  fmt.Println("Enter number of tickets:")
+  fmt.Scan(&userTickets)
+
+  remainingTickets = remainingTickets - userTickets
+  bookings = append(bookings, firstName + " " + lastName)
+
+  fmt.Printf("The whole array: %v\n", bookings)
+  fmt.Printf("The first value: %v\n", bookings[0])
+  fmt.Printf("Slice type: %T\n", bookings)
+  fmt.Printf("Slice length %v\n", len(bookings))
+
+  fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email )
+  fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
 
 // 40:59
