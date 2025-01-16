06a184509ccde8c48b13bb20d3ce164f8794ed2b
CosmoCarpincho <cosmocarpincho@gmail.com>
2025-01-16 00:00:55 -0300

    study: go

---
+++ 01-aprende-go-freeCodeCamp/03-calcular-promedio-cvs.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Alumno struct {
+	Nombre string
+	Notas  []float64
+}
+
+func leerCSV(nombreArchivo string) ([]Alumno, error) {
+	archivo, err := os.Open(nombreArchivo)
+
+	if err != nil {
+		return nil, err
+	}
+
+	lector := csv.NewReader(archivo)
+	lector.Comma = ';'
+
+	registros, err := lector.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var alumnos []Alumno
+
+	for _, registro := range registros {
+		nombre := registro[0]
+		var notas []float64
+		for _, notaStr := range registro[1:] {
+			nota, err := strconv.ParseFloat(notaStr, 64)
+			if err != nil {
+				return nil, err
+			}
+			notas = append(notas, nota)
+		}
+		alumnos = append(alumnos, Alumno{Nombre: nombre, Notas: notas})
+	}
+
+	return alumnos, nil
+
+}
+
+func calcularPromedio(notas []float64) float64 {
+	var suma float64
+	for _, nota := range notas {
+		suma += nota
+	}
+	return suma / float64(len(notas))
+}
+
+func calcularMediaAritmetica(alumnos []Alumno) float64 {
+	var suma float64
+	var cantidadNotas int
+	for _, alumno := range alumnos {
+		for _, nota := range alumno.Notas {
+			suma += nota
+			cantidadNotas++
+		}
+	}
+	return suma / float64(cantidadNotas)
+}
+
+func escribirCSV(nombreArchivo string, alumnos []Alumno) error {
+	archivo, err := os.Create(nombreArchivo)
+	if err != nil {
+		return err
+	}
+
+	defer archivo.Close()
+
+	escritor := csv.NewWriter(archivo)
+
+	defer escritor.Flush()
+
+	for _, alumno := range alumnos {
+		promedio := calcularPromedio(alumno.Notas)
+		registro := []string{alumno.Nombre, fmt.Sprintf("%.2f", promedio)}
+		if err := escritor.Write(registro); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func main() {
+
+	alumnos, err := leerCSV("notas.csv")
+
+	if err != nil {
+		fmt.Println("Error al leer el archivo CSV")
+		return
+	}
+
+	mediaAritmetica := calcularMediaAritmetica(alumnos)
+	fmt.Printf("La media aritmética de todas las notas es: %.2f\n", mediaAritmetica)
+
+	err = escribirCSV("promedios.csv", alumnos)
+	if err != nil {
+		fmt.Println("Error al escribir el archivo CSV:", err)
+		return
+	}
+
+	fmt.Println("Archivo 'promedios.csv' generado con éxito.")
+
+}
---
+++ 03-tutorial-nana/notas-rapidas.md
@@ -0,0 +1,42 @@
+#### ¿Cuándo fue creado?
+
+2007 por google
+2009 open source
+
+#### ¿Por qué se creo?
+
+Se necesitaba un lenguaje que permita escalar las nuevas infraestructuras.
+Ejecutar tareas en paraleleo. (Thread A Thread B ...) -> Multi-threading.
+
+
+#### ¿Qué es la concurrencia (concurrency)?
+
+la concurrencia consiste en tratar con muchas cosas a la vez. (Da el ejemplo de dos personas queriendo comprar el ultimo ticket)
+
+Y que no se generen conflictos cuando se ejecutan tareas en paralelo.
+
+
+Go fue diseñado para este proposito. Tareas en paralelo y concurrencia. (go routines)
+
+
+**Casos de uso:**
+- For Performant Applications 
+- Running on scaled, distributed systems
+
+**Ej:**
+
+- Microservices
+- Web Applications
+- Database Services
+
+- Docker
+- HashiCorp Vault
+- Kubernetes
+- Cockroach db
+
+
+- GO es rapido.
+- Es compilado
+- Multiplataforma
+
+Usado para crear aplicaciones de automatización simples, y CLI para DevOPs y SRE.
