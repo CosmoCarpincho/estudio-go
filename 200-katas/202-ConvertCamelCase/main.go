f450a6df78d536dc976279c6367fdccc1a3828f9
CosmoCarpincho <cosmocarpincho@gmail.com>
2025-02-15 20:27:19 -0300

    study go

---
+++ 200-katas/202-ConvertCamelCase/main.go
@@ -0,0 +1,32 @@
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+// Courtesy of projecteuler.net (Problem 1)
+// https://www.codewars.com/kata/517abf86da9663f1d2000003/train/go
+
+
+package main
+
+import (
+  "fmt"
+)
+
+func ToCamelCase(s string) string {
+
+  buffer := ""
+
+  for k, v := range s {
+
+  }
+
+  return s
+
+}
+
+func main() {
+
+  resultado :=  ToCamelCase("The_Stealth_Warrior")
+  fmt.Println(resultado)
+
+
+}
